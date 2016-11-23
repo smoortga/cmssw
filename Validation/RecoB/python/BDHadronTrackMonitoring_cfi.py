536e848cc2946b05e118ab664debff0f8394722a
smoortga <semoortg@vub.ac.be>
2016-11-23 10:52:26 +0100

    Cleanup

--- Validation/RecoB/python/BDHadronTrackMonitoring_cfi.py
+++ Validation/RecoB/python/BDHadronTrackMonitoring_cfi.py
@@ -4,28 +4,6 @@ import FWCore.ParameterSet.Config as cms
 
 BDHadronTrackMonitoringAnalyze = cms.EDAnalyzer("BDHadronTrackMonitoringAnalyzer",
 								JetSource = cms.InputTag('ak4PFJets')
-								#PatJetSource = cms.InputTag('selectedPatJets')
-								#pfCandidates = cms.InputTag('particleFlow'),
-        						#pvSource = cms.InputTag('offlinePrimaryVertices'),
-        						#svSource = cms.InputTag('inclusiveCandidateSecondaryVertices'),
-        						#muSource = cms.InputTag('muons'),
-        						#elSource = cms.InputTag('gedGsfElectrons'),
-        						#btagInfos = bTagInfos = [
-    							#			'pfImpactParameterTagInfos'
-   								#			,'pfSecondaryVertexTagInfos'
-   								#			,'pfInclusiveSecondaryVertexFinderTagInfos'
-   								#			,'pfSecondaryVertexNegativeTagInfos'
-   								#			,'pfInclusiveSecondaryVertexFinderNegativeTagInfos'
-   								#			,'softPFMuonsTagInfos'
-   								#			,'softPFElectronsTagInfos'
-   								#			,'pfInclusiveSecondaryVertexFinderCvsLTagInfos'
-   								#			,'pfInclusiveSecondaryVertexFinderNegativeCvsLTagInfos'
-								#],
-        						#btagDiscriminators = []
-        						#jetCorrections = ('AK4PF', ['L1FastJet', 'L2Relative', 'L3Absolute'], 'None'),
-        						#genJetCollection = cms.InputTag('ak4GenJetsNoNu'),
-        						#genParticles = cms.InputTag('genParticles'),
-        						#trackSource = cms.InputTag('generalTracks')
                                 )
 
 
