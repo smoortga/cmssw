830d84f30489e86c0daf29c9b0de3b440d317aa2
smoortga <semoortg@vub.ac.be>
2016-12-07 12:49:28 +0100

    Included jet content in harvester

--- Validation/RecoB/plugins/BDHadronTrackMonitoringHarvester.h
+++ Validation/RecoB/plugins/BDHadronTrackMonitoringHarvester.h
@@ -11,13 +11,26 @@
  */
 
 class BDHadronTrackMonitoringHarvester : public DQMEDHarvester {
-   public:
-      explicit BDHadronTrackMonitoringHarvester(const edm::ParameterSet& pSet);
-      ~BDHadronTrackMonitoringHarvester();
+   	public:
+      	explicit BDHadronTrackMonitoringHarvester(const edm::ParameterSet& pSet);
+      	~BDHadronTrackMonitoringHarvester();
 
-   private:
-      void dqmEndJob(DQMStore::IBooker &, DQMStore::IGetter &) override;
-      void beginJob();
+    private:
+      	void dqmEndJob(DQMStore::IBooker &, DQMStore::IGetter &) override;
+      	void beginJob();
+    
+   
+		// Histograms
+		// b jets
+		MonitorElement *nTrk_absolute_bjet, *nTrk_relative_bjet; // number of selected tracks (absolute or in percents) in different track history categories for b jets
+
+		// c jets
+		MonitorElement *nTrk_absolute_cjet, *nTrk_relative_cjet; // number of selected tracks (absolute or in percents) in different track history categories for c jets
+
+
+		// dusg jets
+		MonitorElement *nTrk_absolute_dusgjet, *nTrk_relative_dusgjet; // number of selected tracks (absolute or in percents) in different track history categories for light jets
+
 
 };
 
